667e737428cd505c64df56a39c3b4b85f8aeb13f
hermezx <159937786+hermezx@users.noreply.github.com>
2025-06-09 01:02:41 +0800

    Capstone Project for coursera initialized, semantic tags and meta tags, some components added

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from './Nav';
+
+const Header = () => {
+    return (
+        <header>
+            <img src="" alt="" />
+            <Nav />
+        </header>
+
+    );
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <ul>
+            <li className="home"></li>
+            <li className="about"></li>
+            <li className="menu"></li>
+            <li className="reservation"></li>
+            <li className="order-online"></li>
+            <li className="login"></li>
+        </ul>
+
+    );
+}
+
+export default Nav;
